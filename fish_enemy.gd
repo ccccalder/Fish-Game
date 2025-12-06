d9bf8d6cbd947a04666bed8afc881038243ad451
Calder S <phatpinguin@gmail.com>
2025-12-06 14:14:24 -0800

    added particles

--- fish_enemy.gd
+++ fish_enemy.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@export var explosionScene: PackedScene
+
 @export var minSpeed: float = 150.0  
 @export var maxSpeed: float = 300.0  
 var speed
@@ -19,3 +21,16 @@ func _process(delta: float) -> void:
 
 func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
 	queue_free()
+
+
+func _on_area_entered(area: Area2D) -> void:# 1. Instantiate the Explosion
+	var explosion = explosionScene.instantiate()
+		
+	explosion.global_position = global_position
+	var particles = explosion.get_node("Particles")
+	get_parent().add_child(explosion)
+	particles.emitting = true
+	if area.is_in_group("bullet"):
+		queue_free()
+		
+	particles.finished.connect(explosion.queue_free)
